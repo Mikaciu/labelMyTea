cc063a1b2411c87f4d2c09f306675b5d20301060
Mikaël Hautin <mikael.hautin@gmail.com>
2015-12-16 00:10:08 +0100

    Update tealabel.py
    SVG output

--- tealabel.py
+++ tealabel.py
@@ -1,41 +1,96 @@
 #!/usr/bin/env python3
-from reportlab.lib.utils import ImageReader
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import cm
-import qrcode
+import urllib3
+import xml.dom.minidom
+from jinja2 import Environment, FileSystemLoader
+import os.path
+import io
+import re
+import pyqrcode
 
+box_dimensions = [60, 75]
+box_margins = [5, 5]
 
-def display_rect(c):
-    dims = [6.0 * cm, 7.5 * cm]
-    for x in [1, 7.5, 14]:
-        for y in [1, 9, 17]:
-            c.rect(x * cm, y * cm, *dims, fill=0)
+base_x = 10
+base_y = 10
+current_x = 0
+current_y = 0
 
-            # centered title
-            c.drawCentredString((x + (dims[0] / cm) / 2) * cm, y * cm + dims[1] - 0.5 * cm, "Titre")
+elements = [i for i in range(3)]
+labels = []
 
-            # centered origin
+http = urllib3.PoolManager()
+data_source_request = http.request('GET', "http://mikael.hautin.fr/fileadmin/media/the/the_2.xml")
+if data_source_request.status != 200:
+    print(data_source_request.status)
+    os._exit(1)
 
-            # type      duration        temperature
+data_source_xml = data_source_request.data.decode('utf-8')
 
-            # ingredients
+with xml.dom.minidom.parseString(data_source_xml) as data_source_dom:
+    i_tea_count = 0
+    for o_current_tea in data_source_dom.getElementsByTagName("tea"):
+        s_name = o_current_tea.getAttribute('name')
 
-            # qr
-            img = qrcode.make('http://www.qwant.fr')
-            image = ImageReader(img._img);
-            c.drawImage(image, ((x + (dims[0] / cm) / 2) - 1.25) * cm, (y + 0.25) * cm, width=2.5 * cm, height=2.5 * cm)
+        dom_shop = o_current_tea.getElementsByTagName('shop')[0]
+        s_origin = dom_shop.getAttribute('name')
+        s_url = dom_shop.getAttribute('url')
 
+        dom_kind = o_current_tea.getElementsByTagName('kind')[0]
+        s_kind = dom_kind.getAttribute('code')
+        s_type = dom_kind.getAttribute('name')
 
-c = canvas.Canvas('myfile.pdf', pagesize=A4)
-display_rect(c)
+        # positioning
+        if (i_tea_count % 3) == 0:
+            current_x = base_x
 
-# width, height = A4
-# print(width, height)
+            if current_y > 0:
+                current_y += box_dimensions[1] + box_margins[1]
+            else:
+                current_y = base_y
+        else:
+            current_x += box_dimensions[0] + box_margins[0]
 
-textobject = c.beginText()
-textobject.setTextOrigin(cm, 2.5 * cm)
-c.drawText(textobject)
+        url = pyqrcode.create(s_url, error='Q')
+        buffer = io.BytesIO()
+        url.svg(buffer, xmldecl=False, svgns=False, scale=2.55)
+        s_qr = buffer.getvalue().decode('UTF-8')
+        print(s_qr)
+        s_qr = s_qr.replace('<svg', '<svg x="18.5mm" y="45mm"')
+        s_qr = re.sub('(width|height)="[^"]+"', '\g<1>="35mm"', s_qr)
+        print(s_qr)
 
-c.showPage()
-c.save()
+        # img2 = qrcode.make('Some data here')
+
+        # <tea name="Racconto di Natale">
+        # 	<kind name="thé noir indien" />
+        # 	<shop name="La Via del Tè" location="Firenze" url="http://www.laviadelte.com" />
+        # 	<ingredientlist>
+        # 		<ingredient name="pomme" />
+        # 		<ingredient name="cannelle" />
+        # 		<ingredient name="clous de girofle" />
+        # 		<ingredient name="coriandre" />
+        # 		<ingredient name="fraise en morceaux" />
+        # 		<ingredient name="souci des jardins" />
+        # 	</ingredientlist>
+        # 	<locationlist>
+        # 		<location name="bureau" />
+        # 	</locationlist>
+        # </tea>
+        labels.append({
+            'x': current_x,
+            'y': current_y,
+            'name': s_name,
+            'type': s_type,
+            'kind': s_kind,
+            'origin': s_origin,
+            'qr': s_qr,
+        })
+
+        i_tea_count += 1
+        break
+
+env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
+template = env.get_template('template.svg.tpl')
+
+with open('labels.svg', 'w', encoding='UTF-8') as f_output:
+    f_output.write(template.render(labels=labels))
